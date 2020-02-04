DirtyUnicorns/android_packages_apps_Settings

Determine if the gesture navigation should be enabled
Change-Id: I1bdc2b6f566407749ebbd74f73de9d0374c8c34f

src/com/android/settings/gestures/SystemNavigationPreferenceController.java
@@ -24,6 +24,8 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 
+import com.android.internal.util.du.Utils;
+
 import com.android.settings.R;
 import com.android.settings.core.BasePreferenceController;
 
@@ -53,9 +55,7 @@ public class SystemNavigationPreferenceController extends BasePreferenceControll
     }
 
     static boolean isGestureAvailable(Context context) {
-        // Skip if the swipe up settings are not available
-        if (!context.getResources().getBoolean(
-                com.android.internal.R.bool.config_swipe_up_gesture_setting_available)) {
+        if (!Utils.hasNavigationBar(context)) {
             return false;
         }
 

rajkale99/platform_packages_apps_Settings

Signed-off-by: RakeshBatra <raakesh.batra@rediffmail.com>

src/com/android/settings/gestures/SystemNavigationPreferenceController.java
@@ -24,6 +24,8 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 
+import com.android.internal.util.colt.ColtUtils;
+
 import com.android.settings.R;
 import com.android.settings.core.BasePreferenceController;
 
@@ -53,9 +55,7 @@ public class SystemNavigationPreferenceController extends BasePreferenceControll
     }
 
     static boolean isGestureAvailable(Context context) {
-        // Skip if the swipe up settings are not available
-        if (!context.getResources().getBoolean(
-                com.android.internal.R.bool.config_swipe_up_gesture_setting_available)) {
+        if (!ColtUtils.hasNavigationBar(context)) {
             return false;
         }
 